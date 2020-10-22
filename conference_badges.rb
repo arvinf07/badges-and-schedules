f95e141e47a26f801bcbc5237b01f2a24e20b2f4
Arvin Fernandez <arvinf07@gmail.com>
2020-10-22 10:56:37 -0400

    commit tree 4f26f5c37ef1b03cb740b6da3457e0c595c5de07

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  attendees.collect {|attendee| badge_maker(attendee)}
+end  
+
+def assign_rooms(attendees)
+  attendees.collect.with_index {|attendee, room_num| "Hello, #{attendee}! You'll be assigned to room #{room_num + 1}!"}
+end  
+
+def printer(attendee)
+  batch_badge_creator(attendees).each do |name|
+    puts name
+  end  
+  assign_rooms(attendees).each {|room| puts room}
+end  
